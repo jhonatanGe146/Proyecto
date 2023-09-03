cfdee220aed591196e707e905260402c6c101ec2
JhonatanGel146 <jhonatangelvis@gmail.com>
2023-09-02 21:08:58 -0500

    1.1

--- sistema/Hotel_Perseo/js/banner.js
+++ sistema/Hotel_Perseo/js/banner.js
@@ -15,7 +15,7 @@ slider.css('margin-left', '-' + 100 + '%');
 function moverD() {
     slider.animate({
         marginLeft: '-' + 200 + '%'
-    }, 600, function() {
+    }, 600, function () {
         $('#caja .section_caja:first').insertAfter('#caja .section_caja:last');
         slider.css('margin-left', '-' + 100 + '%');
     });
@@ -25,7 +25,7 @@ function moverD() {
 function moverZ() {
     slider.animate({
         marginLeft: 0
-    }, 600, function() {
+    }, 600, function () {
         $('#caja .section_caja:last').insertBefore('#caja .section_caja:first');
         slider.css('margin-left', '-' + 100 + '%');
     });
@@ -33,20 +33,20 @@ function moverZ() {
 
 // Función para mover las imágenes automáticamente cada 5 segundos
 function autoplay() {
-    interval = setInterval(function() {
+    interval = setInterval(function () {
         moverD();
     }, 6000);
 }
 
 // Función para mover las imágenes a la derecha a través de la pulsación del botón
-siguiente.on('click', function() {
+siguiente.on('click', function () {
     moverD();
     clearInterval(interval);
     autoplay();
 });
 
 // Función para mover a la izquierda
-atras.on('click', function() {
+atras.on('click', function () {
     moverZ();
     clearInterval(interval);
     autoplay();
