6b6c3836a3cbe08c49379cbcafef56ed796e356f
JhonatanGel146 <jhonatangelvis@gmail.com>
2023-09-10 14:19:32 -0500

    1.4

--- sistema/Hotel_Perseo/js/sistema.js
+++ sistema/Hotel_Perseo/js/sistema.js
@@ -16,3 +16,23 @@ function toggleItem(item) {
         contBebida.style.display = 'none';
     }
 }
+
+// Seleccionar el elemento por defecto al cargar la página
+window.addEventListener('DOMContentLoaded', function () {
+    const elementoPorDefecto = document.querySelector('.item');
+    toggleItem(elementoPorDefecto);
+});
+
+
+function seleccionar(link) {
+    // Obtén todos los enlaces del menú
+    let links = document.querySelectorAll("header nav a");
+
+    // Elimina la clase "selected" de todos los enlaces
+    links.forEach(function (item) {
+        item.classList.remove("selected");
+    });
+
+    // Agrega la clase "selected" al enlace que se ha hecho clic
+    link.classList.add("selected");
+}
